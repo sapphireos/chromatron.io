6b43e51d2a620caffef196cb85bebf182e7f0a5d
Christopher Pond <chris@pond.design>
2023-07-30 18:36:25 -0500

    streamline cfg

--- remix.config.js
+++ remix.config.js
@@ -1,15 +1,7 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  ignoredRouteFiles: ["**/.*"],
-  // When running locally in development mode, we use the built-in remix
-  // server. This does not understand the vercel lambda module format,
-  // so we default back to the standard build output.
-  server: process.env.NODE_ENV === "development" ? undefined : "./server.ts",
-  serverBuildPath: "api/index.js",
   tailwind: true,
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // publicPath: "/build/",
+  ignoredRouteFiles: ["**/.*"],
   serverModuleFormat: "cjs",
   future: {
     v2_dev: true,
