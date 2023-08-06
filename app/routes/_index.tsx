eb4f08965f8508ae4d35217972b486dbc009ee12
Christopher Pond <chris@pond.design>
2023-08-06 14:58:59 -0500

    hide all sections to identify render issue

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,14 +2,14 @@ import { type V2_MetaFunction } from "@remix-run/node";
 
 import Template from "~/components/Template";
 
-import { Intro } from "../sections/Intro";
-import { TechnicalDetails } from "../sections/TechnicalDetails";
-import { OpenSource } from "../sections/OpenSource/OpenSource";
-import { AboutUs } from "../sections/AboutUs";
+// import { Intro } from "../sections/Intro";
+// import { TechnicalDetails } from "../sections/TechnicalDetails";
+// import { OpenSource } from "../sections/OpenSource/OpenSource";
+// import { AboutUs } from "../sections/AboutUs";
 // import { ApiExamples } from "../sections/ApiExamples";
-import { Features } from "../sections/Features";
+// import { Features } from "../sections/Features";
 // import { CustomScripts } from "../sections/CustomScripts";
-import { Hardware } from "../sections/Hardware/Hardware";
+// import { Hardware } from "../sections/Hardware/Hardware";
 // import { KickstarterVideo } from "../sections/KickstarterVideo";
 import { Hero } from "../sections/Hero/Hero";
 
@@ -28,15 +28,15 @@ export default function Index() {
   return (
     <Template>
       <Hero />
-      <Intro />
-      <Features />
+      {/* <Intro /> */}
+      {/* <Features /> */}
       {/* <ApiExamples /> */}
       {/* <CustomScripts /> */}
-      <Hardware />
+      {/* <Hardware /> */}
       {/* <KickstarterVideo /> */}
-      <TechnicalDetails />
-      <AboutUs />
-      <OpenSource />
+      {/* <TechnicalDetails /> */}
+      {/* <AboutUs /> */}
+      {/* <OpenSource /> */}
     </Template>
   );
 }
