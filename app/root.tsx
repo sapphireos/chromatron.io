9a223c0bc11509830d965b48656b7611e3f584b7
Christopher Pond <chris@pond.design>
2023-08-06 15:36:15 -0500

    https://remix.run/docs/en/main/route/meta-v2#global-meta

--- app/root.tsx
+++ app/root.tsx
@@ -94,8 +94,6 @@ export const links: LinksFunction = () => [
 
 export const meta: V2_MetaFunction = () => {
   return [
-    { charset: "utf-8" },
-    { name: "viewport", content: "width=device-width,initial-scale=1" },
     {
       property: "og:url",
       content: "https://chromatron.io/",
@@ -148,6 +146,11 @@ export default function App() {
   return (
     <html lang="en">
       <head>
+        <meta charSet="utf-8" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1"
+        />
         <Meta />
         <Links />
       </head>
