c5457e5f8707eb9e14a94add2acd434ca3a25d0a
Christopher Pond <chris@pond.design>
2023-08-06 15:41:51 -0500

    add sitemap (#15)
    * add sitemap
    
    * Update app/routes/[sitemap.xml].tsx

---
+++ app/routes/[sitemap.xml].tsx
@@ -0,0 +1,26 @@
+const buildTime = new Date().toISOString();
+
+export const loader = () => {
+  const content = `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://chromatron.io/</loc>
+    <lastmod>${buildTime}</lastmod>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>https://chromatron.io/privacy</loc>
+    <lastmod>${buildTime}</lastmod>
+    <priority>1.0</priority>
+  </url>
+</urlset>
+    `;
+
+  return new Response(content, {
+    status: 200,
+    headers: {
+      "Content-Type": "application/xml",
+      "xml-version": "1.0",
+      encoding: "UTF-8",
+    },
+  });
+};
